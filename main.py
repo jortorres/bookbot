b2f0a9e58ea2025adcb11d9c12a2180c570191cd
jortorres <jordan.torres1@gmail.com>
2025-04-01 08:24:06 -0500

    added sort_on, sort_list func, update main.py to print out nicely.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import get_num_words, char_count
+from stats import get_num_words, char_count, sort_list
 
 
 def get_book_text(self): # function to get text from .txt file and put into string
@@ -8,11 +8,22 @@ def get_book_text(self): # function to get text from .txt file and put into stri
     return file_contents  # return string
        
 def main():  # main funciton
-   # call number_of_words function with get_book_text function to get word count
-   num_words = get_num_words(get_book_text("books/frankenstein.txt")) 
-   num_char = char_count(get_book_text("books/frankenstein.txt"))
-      
-   print(f"{num_words} words found in the document")
-   print(f"{num_char} characters ")
+
+   num_words = get_num_words(get_book_text("books/frankenstein.txt")) #get num of words
+   num_char = char_count(get_book_text("books/frankenstein.txt")) #get num of chars
+   new_result = []   
+   
+   print("============ BOOKBOT ============")
+   print("Analyzing book found at books/frankenstein.txt...")
+   print("----------- Word Count ----------")
+   print(f"Found {num_words} total words")
+   print(f"--------- Character Count -------")
+   new_result = sort_list(num_char) # get sorted list from dictionary
+
+   for new in new_result:  # print nicely and remove alphabetical chars
+       if new["character"].isalpha():
+          print(f"{new['character']}: {new['count']}")
+   print("============= END ===============")
+
 
 main()  # main call
--- stats.py
+++ stats.py
@@ -1,18 +1,28 @@
-def get_num_words(self): # funciton to count words in file
+def get_num_words(num): # funciton to count words in file
     count = 0 # track words
-    for i in self.split(): # takes string, splits into words, counts each word.
+    for i in num.split(): # takes string, splits into words, counts each word.
         count += 1
     return count
 
-def char_count(self): # function to count characters, put in dictionary
+def char_count(char): # function to count characters, put in dictionary
     dic_char_count = {} # init dictionary
-    count = 0
-    for char_count in self: # loop each chars in string
+    
+    for char_count in char: # loop each chars in string
         char_lower = char_count.lower() # make all lower case
         if char_lower in dic_char_count: # check if char
             dic_char_count[char_lower] += 1 # add 1 to the value in the dictionary
         else:
             dic_char_count[char_lower] = 1 # else if new add 1
-    #print(dic_char_count)
-
     return dic_char_count
+
+def sort_on(dict): # return key
+    return dict["count"]
+
+def sort_list(dict): # sort dictionary by key
+    resultList = []
+    for key, val in dict.items():
+        char_dict = {"character": key, "count": val} # put in new list
+        resultList.append(char_dict) # add to list
+    resultList.sort(reverse=True, key=sort_on) # sort
+      
+    return resultList
