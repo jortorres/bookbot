17a7bdd10f30af1b4f6d32f0b5b468697f7f1da0
jortorres <jordan.torres1@gmail.com>
2025-03-31 21:39:06 -0500

    added function char count

--- main.py
+++ main.py
@@ -13,6 +13,6 @@ def main():  # main funciton
    num_char = char_count(get_book_text("books/frankenstein.txt"))
       
    print(f"{num_words} words found in the document")
-   print(f"{num_char} characters")
+   print(f"{num_char} characters ")
 
 main()  # main call
--- stats.py
+++ stats.py
@@ -4,13 +4,15 @@ def get_num_words(self): # funciton to count words in file
         count += 1
     return count
 
-def char_count(self):
-    dic_char_count = {}
+def char_count(self): # function to count characters, put in dictionary
+    dic_char_count = {} # init dictionary
+    count = 0
+    for char_count in self: # loop each chars in string
+        char_lower = char_count.lower() # make all lower case
+        if char_lower in dic_char_count: # check if char
+            dic_char_count[char_lower] += 1 # add 1 to the value in the dictionary
+        else:
+            dic_char_count[char_lower] = 1 # else if new add 1
+    #print(dic_char_count)
 
-    for char_count in self:
-        char_lower = char_count.lower()
-        dic_char_count[char_lower] = 1
-
-    print(dic_char_count)
-
-    return None 
+    return dic_char_count
